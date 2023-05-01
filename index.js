be1bf5a0580967b8abfeab0b5373e2caa38318df
ksXV <msgcipi@gmail.com>
2023-05-01 10:08:24 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+require("dotenv").config();
+app.get("/foo", (_req, res) => {
+	res.cookie(
+		"foo",
+		"bar",
+		{ httpOnly: true, sameSite: "none", domain: process.env.DOMAIN },
+		{
+			expires: new Date(Date.now() + 25892000000), // set expiry of 1month
+		}
+	);
+});
+
+const server = http.createServer(app);
+server.listen(3000, () => {
+	console.log("we are up and live");
+});
